6906eda157b16138edcd13f9fa6af799aff685a2
MsMinion <wolfgirl2004@hotmail.com>
2017-08-26 20:13:03 -0400

    Set up game layout. Created player selection logic and jQuery effects.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,146 @@
-console.log("Hello world in javaScript!");
 
-function Character(name, hp, attackPower, status, attack, increaseAttack) {
+function Fighter(name, movie, hp, attackPower, status, attack) {
     this.name = name;
+    this.movie = movie;
     this.hp = hp;
-    this.status = status;
-    this.attack = attack(){
+    this.attackPower = attackPower;
+    this.status = function() {
+    //    if(/*player selects Fighter to be their character*/){
+            /*return "player" as status */
+    //    } else if (/*player selects Fighter to be Defender || Fighter is the only enemy left*/){
+            /*return "defender" as status */
+    //    } else{
+            /*return "enemy" as status */
+    //    }
+    }
+    this.attack = function() {
+            
+            //decrease defender or player hp by current attackPower
+            //needs to be an on click trigger
+            //Increase player attackPower by 8 each time event is triggered
+        };
+
+    } //End Character object constructor
+
+
+//All fighters defined as objects
+
+var jacob = new Fighter("Jacob Black", "Twilight Series", 80, 5);
+var lupin = new Fighter("Professor Remus Lupin", "Harry Potter Series", 100, 10);
+var larry = new Fighter("Larry \"The Wolf Man\" Talbot", "The Wolf Man (1941)", 120, 15);
+var david = new Fighter("David Kessler", "An American Werewolf in London", 140, 20);
+
+$("document").ready(function() {
+
+    var currentPlayer;
+
+//Player Selector
+//DRY OUT CODE if you have time. Find a selector that will insert whatever's clicked.
+    console.log("currentPlayer before: " + currentPlayer);
+
+if(currentPlayer === undefined) {
+
+    $("#jacob-div").one("click", function () {
+        var toBeEnemies = $("div[class^=fighter]").not(this);
+        console.log(toBeEnemies);
+        toBeEnemies.removeClass("fighter-status-div").addClass("enemy-status-div");
+        $("#enemy-section-div").append(toBeEnemies);
+        //this.removeClass("fighter-status-div").addClass("player-status-div");        
         
-        if(this.status == "defender"){
-            //decrease defender status by current attack amount
-            //remember it needs to be an on click trigger
-    };
-
-    this.increaseAttack = increaseAttack(){
+        currentPlayer = jacob;
+        console.log("currentPlayer after: " + currentPlayer);
+        console.log("currentPlayer name: " + currentPlayer.name);
         
-        if(/*Player attacked enemy || enemy attacked player*/){
-        return 10 + this.attack;
-        }
-    };
-} //End Character object constructor
+        
+    });
+    
+    $("#lupin-div").one("click", function () {
+        var toBeEnemies = $("div[class^=fighter]").not(this);
+        console.log(toBeEnemies);
+        toBeEnemies.removeClass("fighter-status-div").addClass("enemy-status-div");
+        $("#enemy-section-div").append(toBeEnemies);
+        
+        currentPlayer = lupin;
+        console.log("currentPlayer after: " + currentPlayer);
+        
 
-var name = new Character()
+    });
 
+    $("#larry-div").one("click", function () {
+        var toBeEnemies = $("div[class^=fighter]").not(this);
+        console.log(toBeEnemies);
+        toBeEnemies.removeClass("fighter-status-div").addClass("enemy-status-div");
+        $("#enemy-section-div").append(toBeEnemies);
+        
+        currentPlayer = larry;
+        console.log("currentPlayer after: " + currentPlayer);
+        
+    });
+
+    $("#david-div").one("click", function () {
+        var toBeEnemies = $("div[class^=fighter]").not(this);
+        console.log(toBeEnemies);
+        toBeEnemies.removeClass("fighter-status-div").addClass("enemy-status-div");
+        $("#enemy-section-div").append(toBeEnemies);
+        
+        currentPlayer = david;
+        console.log("currentPlayer after: " + currentPlayer);
+        console.log("currentPlayer name: " + currentPlayer.name);
+        
+        
+    });
+
+} else{
+    console.log("Current player wasn't equal to undefined.");
+} //End If statement for player selector
+
+//Defender Selector
+//DRY OUT CODE if you have time. Find a selector that will insert whatever's clicked.
+/*    
+$("#jacob-div").one("click", function () {
+    console.log("this function works");
+    var toBeDefender = $(this);
+    console.log("Defender is " + toBeDefender);
+    toBeDefender.removeClass("enemy-status-div").addClass("defender-status-div");
+    $("#defender-section-div").append(toBeDefender);
+    
+    console.log(currentPlayer);
+    
+    
+});
+*/
+
+
+//$(“p[id^=para][lang*=en-]”).css
+
+
+}); //End document.ready
+
+
+
+/*
+
+Theme: Famous Werewolves
+Players: 
+    Jacob Black (Twilight Series)
+    David Kessler ( An American Werewolf in London )
+    Larry Talbot (The Wolf Man (1941))
+    Remus Lupin (Harry Potter Series)
+
+//Pseudo Code
+
+1) Set up a "start mode" for the game
+    a) Display all characters on the screen with name, picture, and hp in one div
+        i) Div should have a green border
+    b) Have the screen prompt "Your Character" below all player pictures
+    c) Further down, should show "Enemies Available to Attack," "Fight Section," an "Attack" button, and "Defender", but all empty
+        ii) If "Attack" button is pressed, a message should pop up stating, "First, choose a character and defender."
+    d) Should display background image and title of your game* 
+
+2) 
+
+
+*Bells and whistles. Do this step only after you finish everything else.
+
+
+*/
